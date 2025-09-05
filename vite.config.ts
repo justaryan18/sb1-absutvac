eaf3ee5c18ced373e173a0e87e6c9d6dc0177df7
Aryan Choudhary <144052438+justaryan18@users.noreply.github.com>
2025-09-05 17:04:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/upifrauddetection/"
+  base: "/upifrauddetection/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
